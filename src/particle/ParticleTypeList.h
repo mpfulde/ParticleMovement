406ba0eac47142c2ed3974ca11d7a525b64675e5
Matthew Fulde <mpfulde@ucdavis.edu>
2022-05-27 16:17:30 -0700

    Added General Type List

---
+++ src/particle/ParticleTypeList.h
@@ -0,0 +1,12 @@
+//
+// Created by Pixel on 5/26/2022.
+//
+
+#ifndef PARTICLEMOVEMENT_PARTICLETYPELIST_H
+#define PARTICLEMOVEMENT_PARTICLETYPELIST_H
+
+namespace Particle{
+    using value_type = int;
+}
+
+#endif //PARTICLEMOVEMENT_PARTICLETYPELIST_H
