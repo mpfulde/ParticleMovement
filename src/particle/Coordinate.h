a17e70d88708e66255b8ed40777db66324f16496
Matthew Fulde <mpfulde@ucdavis.edu>
2022-05-27 17:22:16 -0700

    Added constructors to Coordinate object

--- src/particle/Coordinate.h
+++ src/particle/Coordinate.h
@@ -13,14 +13,19 @@ namespace Particle{
     using value_type = value_type;
 
     public:
-
+        Coordinate() = delete;
+        Coordinate(int x, int y);
 
         const value_type & getX() const;
         const value_type & getY() const;
+
     private:
         value_type x;
         value_type y;
     };
+
+    const Coordinate o_coord(0, 0);
 }
+
 
 #endif //PARTICLEMOVEMENT_COORDINATE_H
