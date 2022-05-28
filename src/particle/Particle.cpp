de61a24efeba3e0255d0a7480b96589f8f14e635
Matthew Fulde <mpfulde@ucdavis.edu>
2022-05-27 17:22:26 -0700

    Added constructors to Particle object

--- src/particle/Particle.cpp
+++ src/particle/Particle.cpp
@@ -4,6 +4,13 @@
 
 #include "Particle.h"
 
-Particle::Particle::Particle(const Coordinate &c) : s_coord(c){
+Particle::Particle::Particle(Helpers::StreamStorage &sstorage) : Particle(o_coord, sstorage) {
+
+}
+
+Particle::Particle::Particle(const Coordinate &c, Helpers::StreamStorage &sstorage) : s_coord(c), s_storage(sstorage) {
+}
+
+Particle::Particle::Particle(Particle::value_type x, Particle::value_type y, Helpers::StreamStorage &sstorage) : Particle(Coordinate(x, y), sstorage){
 
 }
--- src/particle/Particle.h
+++ src/particle/Particle.h
@@ -5,27 +5,30 @@
 #ifndef PARTICLEMOVEMENT_PARTICLE_H
 #define PARTICLEMOVEMENT_PARTICLE_H
 
+#include "StreamStorage.h"
 #include "ParticleMovementSystem.h"
 #include "ParticleTypeList.h"
 
 namespace Particle {
     class Coordinate;
 
-
     class Particle {
         using value_type = value_type;
 
     public:
-        Particle();
-        explicit Particle(const Coordinate& c);
-        Particle(value_type x, value_type y);
+        Particle() = delete;
+        explicit Particle(Helpers::StreamStorage& sstorage);
+        Particle(const Coordinate& c, Helpers::StreamStorage& sstorage);
+        Particle(value_type x, value_type y, Helpers::StreamStorage& sstorage);
     private:
-        Physics::ParticleMovementSystem& system;
-        const Coordinate s_coord;
+        Physics::ParticleMovementSystem system;
+        const Coordinate& s_coord;
+        Helpers::StreamStorage& s_storage;
     };
 }
 
 
 #include "Coordinate.h"
+#include "ConfigurationValidation.h"
 
 #endif //PARTICLEMOVEMENT_PARTICLE_H
