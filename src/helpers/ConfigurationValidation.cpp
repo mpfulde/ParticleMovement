748616a793a8b47a95198ac3fd5af66ce88e3d8d
Matthew Fulde <mpfulde@ucdavis.edu>
2022-05-27 17:21:39 -0700

    Added input validation helper

---
+++ src/helpers/ConfigurationValidation.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Pixel on 5/27/2022.
+//
+
+#include "ConfigurationValidation.h"
+
+void Helpers::configMotionSystem(Physics::MovementSystem &system, StreamStorage &s_storage) {
+    return;
+}
---
+++ src/helpers/ConfigurationValidation.h
@@ -0,0 +1,15 @@
+//
+// Created by Pixel on 5/27/2022.
+//
+
+#ifndef PARTICLEMOVEMENT_CONFIGURATIONVALIDATION_H
+#define PARTICLEMOVEMENT_CONFIGURATIONVALIDATION_H
+
+#include "ParticleMovementSystem.h"
+#include "StreamStorage.h"
+
+namespace Helpers{
+    void configMotionSystem(Physics::MovementSystem &system, StreamStorage &s_storage);
+}
+
+#endif //PARTICLEMOVEMENT_CONFIGURATIONVALIDATION_H
